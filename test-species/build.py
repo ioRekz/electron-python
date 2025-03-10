ee101bb1c401edc1021eadec9905afc42ca42d2d
Jeremy Vuillermet <jeremy.vuillermet@gmail.com>
2025-03-10 18:01:02 +0100

    collect tensor

--- test-species/build.py
+++ test-species/build.py
@@ -19,6 +19,8 @@ def build_executable():
             '--distpath', output_dir,
             '--workpath', './build',
             '--copy-metadata', 'cloudpathlib',
+            '--hidden-import', 'tensorflow',
+            '--collect-all', 'tensorflow',
             'main.py'
         ])
 
